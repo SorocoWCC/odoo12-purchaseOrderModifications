d357f135963d1c456a3c7f4b26cf784d5fe16b13
wcastrocarvajal <wcastrocarvajal@users.noreply.github.com>
2019-12-21 13:24:40 -0600

    Elimina precio venta informe

--- models/product.py
+++ models/product.py
@@ -6,6 +6,5 @@ class product(models.Model):
     _name = 'product.template'
     _inherit = 'product.template'
     sumar_validacion = fields.Boolean (string='NO sumar en validación:')
-    precio_venta_informe = fields.Float (string = 'Precio de Venta Informe:')
     calcular = fields.Boolean (string='Calcular peso en ordenes de compra:', default=True) 
 	
